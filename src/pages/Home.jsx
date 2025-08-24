98e7dc5d87e5c892a7daf49f1084fd76347fbca1
nishi <sharminnishi2424@gmail.com>
2025-08-24 19:59:20 +0600

    features option created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import CarouselSlide from '../Components/Carousel/CarouselSlide';
 import HeroCarousel from '../Components/Carousel/HeroCarousel';
+import Features from '../Components/Features';
 
 const Home = () => {
     return (
         <div>
             <HeroCarousel/>
+            <Features/>
         </div>
     );
 };
