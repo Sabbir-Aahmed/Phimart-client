0e4cd3fee78a98cd47ab0bcdc2152b212c5347fc
nishi <sharminnishi2424@gmail.com>
2025-08-24 22:51:32 +0600

    32.3 Creating Loading Spinner, Error & Services

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -3,43 +3,67 @@ import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 import { Navigation } from "swiper/modules";
 import { SwiperSlide, Swiper } from "swiper/react";
-
+import apiClient from "../../Services/api-client";
 import "swiper/css";
 import "swiper/css/navigation";
+import ErrorAlert from "./ErrorAlert";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState("")
 
   useEffect(() => {
-    axios
-      .get("https://phimart-psi.vercel.app/api/v1/products/")
-      .then((res) => setProducts(res.data.results));
+    setLoading(true)
+    apiClient
+      .get("/products/")
+      .then((res) => setProducts(res.data.results))
+      .catch(err => setError(err.message))
+      .finally(() => setLoading(false))
+      
   }, []);
   return (
     <section className="mx-auto bg-gray-50 py-16">
 
-        <div className="flex justify-between items-center px-4 md:px-8">
+        <div className="flex justify-between items-center px-4 md:px-8 mb-8">
             <h2 className="text-3xl md:text-4xl font-bold">Trending Products</h2>
             <a href="#" className="btn btn-secondary px-6 py-6 rounded-full text-lg">View All</a>
         </div>
+
+        {/* loading spinner */}
+        {isLoading && (
+            <div className="flex justify-center items-center py-10">
+                <span className="loading loading-spinner text-secondary loading-xl "></span>
+            </div>
+        )}
+
+        {/* error alert  */}
+        {error && <ErrorAlert error={error}/>}
+
         {/* Product slider */}
-        <Swiper
-            modules={[Navigation]}
-            spaceBetween={10}
-            slidesPerView={1}
-            navigation
-            breakpoints={{
-                640: {slidesPerView: 2},
-                1024: {slidesPerView: 3},
-            }}
-            className="mt-4 px-4 container"
-            >
-            {products.map((product) => (
-                <SwiperSlide key={product.id} className="flex justify-center">
-                <ProductCard key={product.key} product={product} />
-                </SwiperSlide>
-            ))}
-        </Swiper>
+        {!isLoading && !error && products.length > 0 &&(
+            <Swiper
+                modules={[Navigation]}
+                spaceBetween={10}
+                slidesPerView={1}
+                navigation
+                breakpoints={{
+                    640: {slidesPerView: 2},
+                    1024: {slidesPerView: 3},
+                }}
+                className="mt-4 px-4 container"
+                >
+                {products.map((product) => (
+                    <SwiperSlide key={product.id} className="flex justify-center">
+                    <ProductCard key={product.key} product={product} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        )}
+
+        {!isLoading && !error && products.length === 0 &&(
+            <p className="text-center text-gray-500 mt-6">No Products Available</p>
+        )}
     </section>
   );
 };
