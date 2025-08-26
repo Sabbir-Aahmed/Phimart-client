deafef439c9ea2b2c030179663002268c583a1f8
nishi <sharminnishi2424@gmail.com>
2025-08-27 00:47:07 +0600

    Show Dynamic Product Using Pagination

--- src/Components/Shop/ShopPage.jsx
+++ src/Components/Shop/ShopPage.jsx
@@ -9,24 +9,48 @@ const ShopPage = () => {
     const [products,setProducts] =useState([])
     const [loading,setLoading] = useState(false)
     const [error, setError] = useState("")
-    const [totalPages, setTotlPages] = useState(0)
+    const [totalPages, setTotalPages] = useState(0)
     const [currentPage, setCurrentPage] = useState(1)
 
     useEffect(() => {
         fetchProduct()
 
-    },[])
+    },[currentPage])
 
-    const fetchProduct = () => {
+    // const fetchProduct = () => {
+    //     setLoading(true);
+    //     apiClient.get(`/products/?page=${currentPage}`)
+    //     .then((res) => {
+    //         setProducts(res.data.results)
+
+    //         const pageSize = res.data.results.length > 0 ? res.data.results.length : 1;
+
+    //         if(currentPage === 1){
+    //             setTotalPages(Math.ceil(res.data.count / pageSize))
+    //         }
+    //     })
+    //     .catch(err => setError(err.message))
+    //     .finally(() => setLoading(false))
+    // }
+
+    const fetchProduct = async() => {
         setLoading(true);
-        apiClient.get('/products')
-        .then((res) => {
-            setProducts(res.data.results)
-            setTotlPages(Math.ceil(res.data.count / res.data.results.length))
-        })
-        .catch(err => setError(err.message))
-        .finally(() => setLoading(false))
+        try{
+            const response = await apiClient.get(`/products/?page=${currentPage}`)
+            const data = await response.data;
+
+            setProducts(data.results)
+            const pageSize = data.results.length > 0 ? data.results.length : 1
+            if(currentPage === 1){
+                setTotalPages(Math.ceil(data.count / pageSize))
+            }
+        }catch(error){
+            setError(error.message)
+        }finally{
+            setLoading(false)
+        }
     }
+
     return (
 
         // product list 
