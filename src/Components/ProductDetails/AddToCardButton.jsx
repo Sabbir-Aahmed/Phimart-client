e91a3a9a949491266d2dddd0e1de1abd00bc16c3
nishi <sharminnishi2424@gmail.com>
2025-09-06 20:20:14 +0600

    Creating a Dynamic Add to Cart Button

--- src/Components/ProductDetails/AddToCardButton.jsx
+++ src/Components/ProductDetails/AddToCardButton.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
+
 import { FaShoppingCart } from 'react-icons/fa';
-import { FaMinus, FaPlus } from 'react-icons/fa6';
+import { FaCheck, FaMinus, FaPlus } from 'react-icons/fa6';
 
 const AddToCardButton = ({product}) => {
     const [quantity, setQuantity] = useState(1)
+    const [isAdding, setIsAdding] = useState(false)
+    const [isAdded, setIsAdded] = useState(false)
 
     const increaseQuantity = () => {
         if(quantity <product.stock){
@@ -14,6 +17,18 @@ const AddToCardButton = ({product}) => {
         if(quantity > 1){
             setQuantity(quantity-1)
         }
+    }
+
+    const addToCart = async () => {
+        setIsAdding(true)
+        setTimeout(() => {
+            setIsAdding(false)
+            setIsAdded(true)
+
+            setTimeout(() => {
+                setIsAdded(false)
+            }, 2000)
+        }, 1000)
     }
     return (
         <div className='space-y-4'>
@@ -41,11 +56,25 @@ const AddToCardButton = ({product}) => {
                     <FaPlus className='h-4 w-4' />
                 </button>
             </div>
-            <button className='btn btn-primary w-full'>
-                <span className='flex items-center'>
+            <button className='btn btn-primary w-full' onClick={addToCart} disabled ={ isAdding || isAdded || product.stock === 0}>
+                {isAdding ? (
+                    <span className='flex items-center'>
+                        <span className='loading loading-spinner loading-sm mr-2'>
+                        </span>
+                            Adding...
+                    </span>
+                ) : isAdded ? (
+                    <span className='flex items-center'>
+                        <FaCheck className='mr-2 h-4 w-4'/>
+                        Added to Cart
+                    </span>
+                ) : (
+                    <span className='flex items-center'>
                     <FaShoppingCart className='mr-2 h-4 w-4'/>
                     Add to Cart
                 </span>
+                )
+            }
             </button>
         </div>
     );
